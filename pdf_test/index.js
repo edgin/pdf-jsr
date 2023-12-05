989f77a63c44cf43cd62c978acbc6e8cf91c16d1
edgin <lulemat@gmail.com>
2023-12-05 23:57:41 +0100

    Add button

--- pdf_test/index.js
+++ pdf_test/index.js
@@ -3,14 +3,23 @@ const app = express()
 const port = 3000
 const test = require('./test.js');
 const mock = require('./mock_data.js');
+const path = require('path');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// sendFile will go here
+app.get('/', function(req, res) {
+  res.sendFile(`${__dirname}/index.html`);
+});
 
 app.use('/mock', mock);
 
 app.use('/test', test);
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
