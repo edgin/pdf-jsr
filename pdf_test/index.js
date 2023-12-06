4752efc27253ec4ecebfb03e2f2b4e77e253c06d
edgin <lulemat@gmail.com>
2023-12-06 13:47:42 +0100

    Unify slips

--- pdf_test/index.js
+++ pdf_test/index.js
@@ -15,11 +15,13 @@ app.get('/', function(req, res) {
   res.sendFile(`${__dirname}/index.html`);
 });
 
+app.get('/mo', function(req, res) {
+  res.sendFile(`${__dirname}/display_item.html`);
+});
+
 app.use('/mock', mock);
 
 app.use('/test', test);
-
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
